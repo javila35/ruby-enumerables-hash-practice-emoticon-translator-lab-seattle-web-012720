f3d61875eb011c6ec9346212114ae482f8751d05
Joseph Avila <josepharthuravila@gmail.com>
2020-01-17 04:50:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_japanese_emoticon(file_path, emoticon)
 end 
 
 def get_english_meaning(file_path, emoticon)
-  load_library(file_path)
+  hash = load_library(file_path)
   if hash["get_meaning"].include?(emoticon)
     return["get_emoticon"][emoticon]
   end 
